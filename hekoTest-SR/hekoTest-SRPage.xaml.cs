3f8be4155ceb98ceb8371d7aae38a991a0411702
BaconCake <saschara@gmail.com>
2017-11-30 09:21:30 +0100

    * AboutAssets.txt: Initial Commit
    * Main.cs:
    * Info.plist:
    * hekoTest-SR.sln:
    * AppDelegate.cs:
    * packages.config:
    * App.xaml:
    * hekoTest-SR.userprefs:
    * MainActivity.cs:
    * packages.config:
    * Entitlements.plist:
    * App.xaml.cs:
    * hekoTest-SR.iOS.csproj:
    * LaunchScreen.storyboard:
    * packages.config:
    * hekoTest-SR.Droid.csproj:
    * hekoTest-SR.csproj:
    * hekoTest-SRPage.xaml:
    * AssemblyInfo.cs:
    * styles.xml:
    * Contents.json:
    * icon.png:
    * AboutResources.txt:
    * Tabbar.axml:
    * hekoTest-SRPage.xaml.cs:
    * Toolbar.axml:
    * Resource.designer.cs:
    * AndroidManifest.xml:
    * icon.png:
    * AssemblyInfo.cs:
    * icon.png:
    * icon.png:
    * Contents.json:

---
+++ hekoTest-SR/hekoTest-SRPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace hekoTestSR
+{
+    public partial class hekoTest_SRPage : ContentPage
+    {
+        public hekoTest_SRPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
